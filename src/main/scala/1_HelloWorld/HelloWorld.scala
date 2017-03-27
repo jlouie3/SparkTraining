7247ce8032c3879ce0ef7637613058e200c72d67
unknown <louie@LouieM4700.rdacorp.com>
2017-03-27 07:54:48 -0400

    updates

--- src/main/scala/1_HelloWorld/HelloWorld.scala
+++ src/main/scala/1_HelloWorld/HelloWorld.scala
@@ -3,6 +3,7 @@
   */
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object HelloWorld {
   def main(args: Array[String]) : Unit = {
@@ -29,6 +30,19 @@ object HelloWorld {
     println(apple.first())
     println("Take all rows and put in an array: ")
     apple.collect().foreach(println)
+
+    //////////////////////////////////////////////////////////////////
+    // REPEAT ABOVE BUT USE SPARKSESSION AND DATAFRAME IN LIEU OF RDD
+    //////////////////////////////////////////////////////////////////
+
+    // Import .csv file to dataframe object
+    val spark = SparkSession.builder().appName(appName).getOrCreate()
+    val df_apple = spark.read.option("header", value = true).csv(filePath)
+
+    // Basic dataframe actions
+    val ten_rows = df_apple.take(10)  // Take top 10 rows and then print 1 on each line
+    ten_rows.foreach(println)
+    println(df_apple.first())         // Take top row and print it
 
   }
 }
--- src/main/scala/2_BasicTransforms/BasicTransforms.scala
+++ src/main/scala/2_BasicTransforms/BasicTransforms.scala
@@ -2,13 +2,13 @@
   * Created by louie on 3/1/2017.
   */
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 object BasicTransforms {
   def main(args: Array[String]) : Unit = {
     // Spark Config
-    val appName = "Hello World"
+    val appName = "Basic Transforms"
     val masterURL = "local[2]"
     val memAlloc = "1g"
     val conf = new SparkConf().setAppName(appName).setMaster(masterURL).set("spark.executor.memory",memAlloc)
@@ -35,7 +35,13 @@ object BasicTransforms {
     val dataAsListRDD = dataWithoutHeader.map(x => x.split(",")).take(10)
     val count = dataWithoutHeader.count()
 
-    dataWithoutHeader.
+    /////////////////////////////////////////////////////////////////
+    // REPEAT ABOVE BUT USE SPARKSESSION AND DATAFRAME IN LIEU OF RDD
+    //////////////////////////////////////////////////////////////////
 
+    val spark = SparkSession.builder().appName(appName).getOrCreate()
+    val df_data = spark.read.option("header", value = true).csv(filePath)
+    df_data.persist()
+    val count_df = df_data.count()
   }
 }
